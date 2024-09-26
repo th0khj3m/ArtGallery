b57f3b8fb503bbeef5895df8c52b27dc59addca5
Khj3m <109201777+lolvuongong@users.noreply.github.com>
2024-09-26 14:36:06 +0700

    Adding mkcert (self-signed certificate)

--- ArtGallery.WebAPI/Program.cs
+++ ArtGallery.WebAPI/Program.cs
@@ -9,7 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});   
+});  
+builder.Services.AddCors();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,6 +24,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Enable CORS
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
